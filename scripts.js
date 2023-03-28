5243b4a519d986382a67b5c039749395977c6a05
Zack-B-code <106704877+Zack-B-code@users.noreply.github.com>
2023-03-28 02:15:45 +0000

    Trying to get a canvas to appear on click of logo

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
+// Get a reference to the existing image element
+const image = document.querySelector('img["Assets/TriplexLogo.png"]');
+
+// Add a click event listener to the image
+image.addEventListener('click', () => {
+  // Create a new canvas element
+  const canvas = document.createElement('canvas');
+  
+  // Set the dimensions of the canvas to 400x400
+  canvas.width = 400;
+  canvas.height = 400;
+  
+  // Set the position of the canvas to the center of the screen
+  canvas.style.position = 'fixed';
+  canvas.style.top = '50%';
+  canvas.style.left = '50%';
+  canvas.style.transform = 'translate(-50%, -50%)';
+  
+  // Add the canvas to the document body
+  document.body.appendChild(canvas);
+
+  const context = canvas.getContext('2d');
+  context.font = '24px Arial';
+  context.fillStyle = 'black';
+  context.textAlign = 'center';
+  context.fillText('This is a pause screen', canvas.width / 2, canvas.height / 2);
+});
+
+
+
+
+
 
