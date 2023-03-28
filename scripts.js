6f284fa9c8c7e77572ec5de0d8cea9ba95df4240
Zack-B-code <106704877+Zack-B-code@users.noreply.github.com>
2023-03-28 15:10:11 +0000

    included .js file in html file, tried changing .js code

--- scripts.js
+++ scripts.js
@@ -1,29 +1,20 @@
-// Get a reference to the existing image element
-const image = document.querySelector('#main-content');
+const image = document.querySelector('img[src="Assets/TriplexLogo.png"]');
+const canvas = document.createElement('canvas');
+canvas.style.position = 'absolute';
+canvas.style.top = '50%';
+canvas.style.left = '50%';
+canvas.style.transform = 'translate(-50%, -50%)';
 
-// Add a click event listener to the image
 image.addEventListener('click', () => {
-  // Create a new canvas element
-  const canvas = document.createElement('canvas');
-  
-  // Set the dimensions of the canvas to 400x400
-  canvas.width = 900;
-  canvas.height = 900;
-  
-  // Set the position of the canvas to the center of the screen
-  canvas.style.position = 'fixed';
-  canvas.style.top = '50%';
-  canvas.style.left = '50%';
-  canvas.style.transform = 'translate(-50%, -50%)';
-  
-  // Add the canvas to the document body
   document.body.appendChild(canvas);
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-  const context = canvas.getContext('2d');
-  context.font = '24px Arial';
-  context.fillStyle = 'purple';
-  context.textAlign = 'center';
-  context.fillText('This is a pause screen', canvas.width / 2, canvas.height / 2);
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'purple';
+  ctx.font = '96px Roboto';
+  ctx.textAlign = 'center';
+  ctx.fillText('This is a pause screen', canvas.width / 2, canvas.height / 2);
 });
 
 
