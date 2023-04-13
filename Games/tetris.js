cf5ebb17a05803d9c5d6ca8f2c81027b62345873
Zack-B-code <106704877+Zack-B-code@users.noreply.github.com>
2023-04-13 03:04:48 +0000

    tetris reset

--- Games/tetris.js
+++ Games/tetris.js
@@ -208,18 +208,19 @@ export function loadTetris(){
       //reset game (added in cause it seems like an important feature)
       context.fillStyle = 'black';
       context.globalAlpha = 0.75;
-      context.fillRect(0, canvas.height / 2 - 30, canvas.width, 60);
+      context.fillRect(0, canvas.height / 1.50 - 30, canvas.width, 60);
     
       context.globalAlpha = 1;
       context.fillStyle = 'white';
       context.font = '36px monospace';
       context.textAlign = 'center';
       context.textBaseline = 'middle';
-      context.fillText('Press the spacebar to restart', canvas.width / 2, canvas.height / 1.75);
+      context.fillText('Press the spacebar to restart', canvas.width / 2, canvas.height / 1.50);
       document.addEventListener('keydown', function(e) {
         if (e.which === 32) {
           gameOver = false;
-          rAF = requestAnimationFrame(loop);
+          raF = requestAnimationFrame(loop);
+          loadTetris()
         }
       });
 
